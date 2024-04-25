0ee85d1b5e91b0382b026c94997ace88efe2a1eb
Ahmad Mekheber <ahmad.y.mekheber@gmail.com>
2024-04-25 17:03:10 +0200

    Finished CRUD Operations without Delete Confirmation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,172 +1,173 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import { ClientService } from "./client.service";
-import { GridModule, GridComponent, GridDataResult, PageChangeEvent, AddEvent, EditEvent, CancelEvent, SaveEvent } from '@progress/kendo-angular-grid';
+import { Component, ViewChild } from '@angular/core';
 import { InputsModule } from '@progress/kendo-angular-inputs';
 import { NgForm, FormsModule } from '@angular/forms';
-import { SortDescriptor } from '@progress/kendo-data-query';
+import { GridModule, GridComponent, AddEvent, EditEvent, CancelEvent, SaveEvent, RemoveEvent } from '@progress/kendo-angular-grid';
 import { CommonModule } from '@angular/common';
-// import { GridDataResult } from './grid-data-result';
-// import { kendoLicense } from '@progress/kendo-licensing';
-import { Observable } from 'rxjs';
+import { RouterOutlet } from '@angular/router';
+import { SortDescriptor } from '@progress/kendo-data-query';
+import { ClientService } from "./client.service";
 import { HttpClientModule } from '@angular/common/http';
-import { Client } from './data.clients';
+import { Client } from './client';
+// import { NotificationService } from '@progress/kendo-angular-notification';
+// import { BrowserModule } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, GridModule, InputsModule, FormsModule, CommonModule, HttpClientModule],
+  imports: [RouterOutlet, CommonModule, GridModule, InputsModule, FormsModule, HttpClientModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
   providers: [ClientService]
 })
 
 export class AppComponent {
-  // constructor() {
-  //   kendoLicense('./kendo-ui-license.txt'); // Replace with actual path
-  // }
-  //private editService: EditService;
+  @ViewChild('clientsGrid') clientsGrid: GridComponent | undefined;
+
   private editedRowIndex?: number = undefined;
-  private editedClient?: Client;
+  public editedClient?: Client;
+  public itemToRemove?: Client;
+  public failureMsg?: string = undefined;
 
   constructor(private service: ClientService) {
     this.loadGridItems();
   }
 
-  public pageChange(event: PageChangeEvent): void {
-    this.skip = event.skip;
-    this.loadGridItems();
-  }
-
-  public handleSortChange(descriptor: SortDescriptor[]): void {
-    this.sortDescriptor = descriptor;
-    this.loadGridItems();
-  }
-
   private loadGridItems(): void {
-    this.gridItems = this.service.getClients(
-      this.skip,
-      this.pageSize,
-      this.sortDescriptor,
-      this.filterTerm
-    );
+    this.service.getClients().subscribe((data) => {
+      this.gridData = data;
 
-    // this.service.getClients(
-    //   this.skip,
-    //   this.pageSize,
-    //   this.sortDescriptor,
-    //   this.filterTerm
-    // ).subscribe((data) => {
-    //   // Access the data here (data is an array of Client objects)
-    //   console.log(data); // Example: log the received data
-
-    //   // You can use the data for further processing or display it in your UI
-    // },
-    // (error) => console.error(`Error fetching clients: ${error}`), // Handle errors
-    // () => console.log('Clients data received successfully')); // Handle completion (optional));
-  }
-
-  public addClient(clientData: any): void {
-    this.service.addClient(clientData).subscribe(() => {
-      this.loadGridItems();
-    });
+      this.failureMsg = undefined;
+    },
+      (error) => console.error(`Error fetching clients: ${error}`), // Handle errors
+      () => console.log('Clients data received successfully')); // Handle completion (optional));
   }
 
   public addHandler(args: AddEvent, formInstance: NgForm): void {
-    console.log(args, "Add Event Args");
-
     formInstance.reset();
+    
     // close the previously edited item
-    this.closeEditor(args.sender);
+    this.closeEditor();
+    
     // open a new item editor
     args.sender.addRow(new Client());
   }
 
   public editHandler(args: EditEvent): void {
-    console.log(args, "Edit Event Args");
-
     // close the previously edited item
-    this.closeEditor(args.sender);
+    this.closeEditor();
+    
     // track the most recently edited row
     // it will be used in `closeEditor` for closing the previously edited row
     this.editedRowIndex = args.rowIndex;
+
     // clone the current - `[(ngModel)]` will modify the original item
     // use this copy to revert changes
     this.editedClient = Object.assign({}, args.dataItem);
+
     // edit the row
     args.sender.editRow(args.rowIndex);
   }
 
   public cancelHandler(args: CancelEvent): void {
-    console.log(args, "Cancel Event Args");
-
-    // call the helper method
-    this.closeEditor(args.sender, args.rowIndex);
+    this.closeEditor();
   }
 
   public saveHandler(args: SaveEvent): void {
-    console.log(args, "Save Event Args");
-    // update the data source
-    //this.editService.save(args.dataItem, args.isNew);
-    
-    if (this.editedClient?.id == null)
-      {
-
-      }
-      else 
-      {
-        this.updateClient(this.editedClient.id, args.dataItem);
-      }
-    
-    // close the editor, that is, revert the row back into view mode
-    //args.sender.closeRow(args.rowIndex);
-
+    if (this.editedClient?.id == null) {
+      this.service.addClient(args.dataItem,
+        (data: any) => this.saveHandlerSuccessCallback(data, args.rowIndex),
+        (error: any) => this.saveHandlerErrorCallback(error)
+      );
+    }
+    else {
+      this.service.updateClient(this.editedClient.id, args.dataItem,
+        (data: any) => this.saveHandlerSuccessCallback(data, args.rowIndex),
+        (error: any) => this.saveHandlerErrorCallback(error)
+      );
+    }
+  }
+  private saveHandlerSuccessCallback(data: any, rowIndex: number): void {
+    this.clientsGrid?.closeRow(rowIndex);
+  
     this.editedRowIndex = undefined;
     this.editedClient = undefined;
+  
+    this.loadGridItems();
+  }
+  private saveHandlerErrorCallback(error: any): void {
+    if (error?.error?.status == 404)
+    {
+      this.failureMsg = error.error.status + " - " + error.error.title;
+    }
+    else 
+    {
+      this.failureMsg = error.error;
+    }
+    console.log(error, "Save Error");
   }
 
-  public updateClient(clientID: number, clientData: any): void {
-    this.service.updateClient(clientID, clientData).subscribe(
-      (data) => {
-        console.log(data, "Save");
-      this.loadGridItems();
-    },
-     (data) => {
-        console.log(data, "Save Error");
-    }, 
-    () => {
-      console.log("Save Complete")
-    });
+  public removeHandler(args: RemoveEvent): void {
+    this.closeEditor();
+
+    this.itemToRemove = args.dataItem;
+
+    this.performRemove(true);
   }
 
-  public deleteClient(clientID: number): void {
-    this.service.deleteClient(clientID).subscribe(() => {
-      this.loadGridItems();
-    });
+  public performRemove(isConfirmed: boolean)
+  {
+    if (isConfirmed && this.itemToRemove?.id)
+      {
+        this.service.deleteClient(this.itemToRemove.id,
+          (data: any) => this.removeClientSuccessCallback(data),
+          (error: any) => this.removeClientErrorCallback(error)
+        )
+      }
+  }
+  private removeClientSuccessCallback(data: any) {
+    this.itemToRemove = undefined;
+
+    this.loadGridItems();
+  }
+  private removeClientErrorCallback(error: any) {
+    this.failureMsg = error.error.status + " - " + error.error.title;
+    console.log(error, "Delete Error");
   }
 
-  private closeEditor(
-    grid: GridComponent,
-    rowIndex = this.editedRowIndex
-  ): void {
+  private closeEditor(): void {
     // close the editor
-    grid.closeRow(rowIndex);
+    this.clientsGrid?.closeRow(this.editedRowIndex);
+
     // revert the data item to original state
-    //
-    //
-    //
-    //this.editService.resetItem(this.editedProduct);
-    //
-    //
-    //
-    // reset the helpers
+    if (this.editedClient)
+      {
+        let originalClient = this.gridData.find(client => client.id == this.editedClient?.id);
+        if (originalClient)
+          {
+            Object.assign(originalClient, this.editedClient);
+          }
+      }
+
     this.editedRowIndex = undefined;
     this.editedClient = undefined;
+    this.failureMsg = undefined;
+  }
+
+  public filterChangeHandler(event: any)
+  {
+    if (this.clientsGrid)
+      this.closeEditor();
+  }
+  
+  public sortChangeHandler(event: any)
+  {
+    if (this.clientsGrid)
+      this.closeEditor();
   }
 
   title = 'ASAP_Clients_UI';
 
-  public gridItems: Observable<any> | undefined;
+  public gridData: Client[] = [];
   public pageSize: number = 10;
   public skip: number = 0;
   public sortDescriptor: SortDescriptor[] = [];
--- src/app/client.service.ts
+++ src/app/client.service.ts
@@ -1,69 +1,31 @@
 import { Injectable } from '@angular/core';
-import { DataResult, orderBy, process, SortDescriptor } from '@progress/kendo-data-query';
-import { Observable, of } from 'rxjs';
-//import { clients } from './clients-generator';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
-import { GridModule, GridDataResult, PageChangeEvent } from '@progress/kendo-angular-grid';
-import { Client } from './data.clients';
+import { Observable } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
+import { Client } from './client';
 
 @Injectable({
     providedIn: 'root'
 })
 export class ClientService {
-    private apiUrl = 'http://localhost:5222/api/Client';
+    private readonly apiUrl = 'http://localhost:5222/api/Client';
 
     constructor(private http: HttpClient) { }
 
-    getClients(skip: number, pageSize: number, sort: any, filter: any): Observable<Client> {
-        // Implement your GET method here to fetch data from the API
-        const params = { skip: skip.toString(), pageSize: pageSize.toString(), sort, filter };
-        return this.http.get<Client>(this.apiUrl);
+    getClients(): Observable<Client[]> {
+        return this.http.get<Client[]>(this.apiUrl);
       }
 
-      addClient(clientData: any): Observable<any> {
-        // Implement your POST method here to add a new client
-        return this.http.post<any>(this.apiUrl, clientData);
+      addClient(clientData: any, callback: any, errorCallback: any): void {
+        this.http.post<any>(this.apiUrl, clientData).subscribe(callback, errorCallback);
       }
 
-      updateClient(clientID: number, clientData: any): Observable<any> {
-        // Implement your PUT method here to update an existing client
+      updateClient(clientID: number, clientData: any, callback: any, errorCallback: any): void {
         const url = `${this.apiUrl}/${clientID}`;
-        return this.http.put<any>(url, clientData);
+        this.http.put<any>(url, clientData).subscribe(callback, errorCallback);
       }
 
-      deleteClient(clientID: number): Observable<any> {
-        // Implement your DELETE method here to delete an existing client
+      deleteClient(clientID: number, callback: any, errorCallback: any): void{
         const url = `${this.apiUrl}/${clientID}`;
-        return this.http.delete<any>(url);
+        this.http.delete<any>(url).subscribe(callback, errorCallback);
       }
-
-
-    // public getClients(
-    //     skip: number,
-    //     pageSize: number,
-    //     sortDescriptor: SortDescriptor[],
-    //     filterTerm: number | null
-    // ): Observable<DataResult> {
-    //     let data;
-    //     if (filterTerm) {
-    //         data = process(orderBy(clients, sortDescriptor), {
-    //             filter: {
-    //                 logic: 'and',
-    //                 filters: [
-    //                     {
-    //                         field: 'ID',
-    //                         operator: 'eq',
-    //                         value: filterTerm
-    //                     }
-    //                 ]
-    //             }
-    //         }).data;
-    //     } else {
-    //         data = orderBy(clients, sortDescriptor);
-    //     }
-    //     return of({
-    //         data: data.slice(skip, skip + pageSize),
-    //         total: data.length
-    //     });
-    // }
 }
